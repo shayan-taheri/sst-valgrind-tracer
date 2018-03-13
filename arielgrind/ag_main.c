6f07b9e3d568db21af3e4843433343381176ce98
Simon David Hammond <sdhammo@sst-devel.sandia.gov>
2018-03-13 09:01:06 -0600

    Add verbosity command line option. Test case.

--- arielgrind/ag_main.c
+++ arielgrind/ag_main.c
@@ -1,6 +1,10 @@
 
 #include "pub_tool_basics.h"
+#include "pub_tool_options.h"
+#include "pub_tool_libcbase.h"
 #include "pub_tool_tooliface.h"
+
+static Int verbose = 0;
 
 static void ag_post_clo_init(void)
 {
@@ -19,6 +23,38 @@ IRSB* ag_instrument ( VgCallbackClosure* closure,
 
 static void ag_fini(Int exitcode)
 {
+	VG_(printf)("== Execution completed, return code: %d.\n", (int) exitcode);
+}
+
+static void ag_print_usage(void)
+{
+   VG_(printf)(
+	"    --verbose=level           Set tool verbosity to level, higher is more verbose.\n"
+   );
+}
+
+static Bool ag_process_cmd_line_option(const HChar* arg)
+{
+//   if VG_STR_CLO(arg, "--fnname", clo_fnname) {}
+//   else if VG_BOOL_CLO(arg, "--basic-counts",      clo_basic_counts) {}
+//   else if VG_BOOL_CLO(arg, "--detailed-counts",   clo_detailed_counts) {}
+//   else if VG_BOOL_CLO(arg, "--trace-mem",         clo_trace_mem) {}
+//   else if VG_BOOL_CLO(arg, "--trace-superblocks", clo_trace_sbs) {}
+//   else
+//      return False;
+
+//   tl_assert(clo_fnname);
+//   tl_assert(clo_fnname[0]);
+//   return True;
+
+	if( VG_INT_CLO( arg, "--verbose", verbose ) ) {}
+
+	return True;
+}
+
+static void ag_print_debug_usage(void)
+{
+   VG_(printf)("\n");
 }
 
 static void ag_pre_clo_init(void)
@@ -36,11 +72,12 @@ static void ag_pre_clo_init(void)
                                  ag_instrument,
                                  ag_fini);
 
+   VG_(needs_command_line_options)(ag_process_cmd_line_option,
+                                   ag_print_usage,
+                                   ag_print_debug_usage);
+
    /* No needs, no core events to track */
 }
 
 VG_DETERMINE_INTERFACE_VERSION(ag_pre_clo_init)
 
-/*--------------------------------------------------------------------*/
-/*--- end                                                          ---*/
-/*--------------------------------------------------------------------*/
